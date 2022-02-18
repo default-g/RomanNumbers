41822a707a24aeaadeba7c040e3377e5e8ea40f8
Artem <arteam_101@mail.ru>
2022-02-18 13:30:36 +0700

    Реализовал класс RomanNumber

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,11 +1,102 @@
 // See https://aka.ms/new-console-template for more information
 
 using System;
+using System.Security.Cryptography;
+
+class RomanNumberException : Exception
+{
+    public RomanNumberException() : base() { }
+    public RomanNumberException(string message) : base(message) { }
+}
+
+internal class RomanNumber : ICloneable, IComparable
+{
+    private readonly ushort number;
+    
+    public RomanNumber(ushort n)
+    {
+        if (n <= 0)
+        {
+            throw new RomanNumberException();
+        }
+        number = n;
+    }
+    public static RomanNumber Add(RomanNumber? n1, RomanNumber? n2)
+    {
+        return new RomanNumber((ushort)(n1.number + n2.number));
+    }
+    public static RomanNumber Sub(RomanNumber? n1, RomanNumber? n2)
+    {
+        if (n1.number <= n2.number) throw new RomanNumberException();
+        return new RomanNumber((ushort)(n1.number - n2.number));
+    }
+    public static RomanNumber Mul(RomanNumber? n1, RomanNumber? n2)
+    {
+        return new RomanNumber((ushort)(n1.number * n2.number));
+    }
+    public static RomanNumber Div(RomanNumber? n1, RomanNumber? n2)
+    {
+        return new RomanNumber((ushort)(n1.number / n2.number));
+    }
+    public override string ToString()
+    {
+        return toRoman(number);
+    }
+    private static string toRoman(int number)
+    {
+        if (number < 1) return string.Empty; 
+        if (number >= 1000) return "M" + toRoman(number - 1000);
+        if (number >= 900) return "CM" + toRoman(number - 900); 
+        if (number >= 500) return "D" + toRoman(number - 500);
+        if (number >= 400) return "CD" + toRoman(number - 400);
+        if (number >= 100) return "C" + toRoman(number - 100);            
+        if (number >= 90) return "XC" + toRoman(number - 90);
+        if (number >= 50) return "L" + toRoman(number - 50);
+        if (number >= 40) return "XL" + toRoman(number - 40);
+        if (number >= 10) return "X" + toRoman(number - 10);
+        if (number >= 9) return "IX" + toRoman(number - 9);
+        if (number >= 5) return "V" + toRoman(number - 5);
+        if (number >= 4) return "IV" + toRoman(number - 4);
+        if (number >= 1) return "I" + toRoman(number - 1);
+        throw new RomanNumberException();
+    }
+
+    public object Clone()
+    {
+        return new RomanNumber(number);
+    }
+
+    public int CompareTo(object obj)
+    {
+        if (obj is RomanNumber num) return number.CompareTo(num.number);
+        else throw new RomanNumberException("Некорректное значение аргумента");
+    }
+    
+}
 
 class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, world!");
+        Console.WriteLine(RomanNumber.Add(new RomanNumber(20), new RomanNumber(10)));
+        Console.WriteLine(RomanNumber.Sub(new RomanNumber(20), new RomanNumber(10)));
+        Console.WriteLine(RomanNumber.Mul(new RomanNumber(20), new RomanNumber(10)));
+        Console.WriteLine(RomanNumber.Div(new RomanNumber(20), new RomanNumber(10)));
+        RomanNumber[] romanNumberArray = new RomanNumber[10];
+        for (int i = 0; i < 10; i++)
+        {
+            romanNumberArray[i] = new RomanNumber((ushort)(new Random().Next(1, 500)));
+            Console.Write(romanNumberArray[i] + " ");
+        }
+        Console.WriteLine();
+
+        Array.Sort(romanNumberArray);
+        
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write(romanNumberArray[i] + " ");
+        }
+
+
     }
 }
